99605c6f9a86c9f72e3fbafe3ae4d49696e913e1
mariogate <marwan.mfahim@gmail.com>
2024-08-19 15:58:04 +0300

    fix

--- config/Database.php
+++ config/Database.php
@@ -7,9 +7,12 @@ $dbname = "newschema";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     error_log("Connection failed: " . $conn->connect_error);
     die("Connection failed: " . $conn->connect_error);
-}die("Connection failed: " . $conn->connect_error);
+}
 
+// Connection successful
+// No need to die() after a successful connection
 ?>
